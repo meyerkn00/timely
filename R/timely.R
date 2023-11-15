6306677c458ab828288c64ebe3799747bb1befbf
meyerkn00 <karl@themeyers.org>
2023-11-15 11:21:49 -0500

    Add documentation
    Also delete skeleton 'hello' function that was created when RStudio made the package

--- R/timely.R
+++ R/timely.R
@@ -1,18 +1,20 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
 #   Install Package:           'Cmd + Shift + B'
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-timely <- function(name = "test", extension = ".csv", time = "_%m%d%y_%H.%M") {
-  paste0(name, format(Sys.time(), format=time), extension)
+#' Timely: Generate a time-stamped filename
+#'
+#' @param name a string with the title of the file
+#' @param extension a file extension, e.g. csv or dta
+#' @param time optional timestamp defintion, see format() for syntax
+#'
+#' @return a string
+#' @export
+#'
+#' @examples
+#' timely()
+#' timely("data_output", "dta", "%m%d%y_%H.%M")
+timely <- function(name = "test", extension = "csv", time = "%m%d%y_%H.%M") {
+
+  paste0(name, "_", format(Sys.time(), format=time), ".", extension)
 }
